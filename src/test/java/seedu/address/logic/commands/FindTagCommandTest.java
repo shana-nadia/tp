512d96d38bfbc3940e4d49407a6c67dc6e2dbc52
yytelliot <yytelliot@gmail.com>
2026-03-25 21:19:37 +0800

    Fix tabs

--- src/test/java/seedu/address/logic/commands/FindTagCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FindTagCommandTest.java
@@ -24,71 +24,71 @@ import seedu.address.model.person.TagsContainKeywordsPredicate;
  * Contains integration tests (interaction with the Model) for {@code FindTagCommand}.
  */
 public class FindTagCommandTest {
-	private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-	private final Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private final Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
-	@Test
-	public void equals() {
-		TagsContainKeywordsPredicate firstPredicate =
-				new TagsContainKeywordsPredicate(Collections.singletonList("friends"));
-		TagsContainKeywordsPredicate secondPredicate =
-				new TagsContainKeywordsPredicate(Collections.singletonList("owesMoney"));
+    @Test
+    public void equals() {
+        TagsContainKeywordsPredicate firstPredicate =
+                new TagsContainKeywordsPredicate(Collections.singletonList("friends"));
+        TagsContainKeywordsPredicate secondPredicate =
+                new TagsContainKeywordsPredicate(Collections.singletonList("owesMoney"));
 
-		FindTagCommand findFirstCommand = new FindTagCommand(firstPredicate);
-		FindTagCommand findSecondCommand = new FindTagCommand(secondPredicate);
+        FindTagCommand findFirstCommand = new FindTagCommand(firstPredicate);
+        FindTagCommand findSecondCommand = new FindTagCommand(secondPredicate);
 
-		assertTrue(findFirstCommand.equals(findFirstCommand));
+        assertTrue(findFirstCommand.equals(findFirstCommand));
 
-		FindTagCommand findFirstCommandCopy = new FindTagCommand(firstPredicate);
-		assertTrue(findFirstCommand.equals(findFirstCommandCopy));
+        FindTagCommand findFirstCommandCopy = new FindTagCommand(firstPredicate);
+        assertTrue(findFirstCommand.equals(findFirstCommandCopy));
 
-		assertFalse(findFirstCommand.equals(1));
+        assertFalse(findFirstCommand.equals(1));
 
-		assertFalse(findFirstCommand.equals(null));
+        assertFalse(findFirstCommand.equals(null));
 
-		assertFalse(findFirstCommand.equals(findSecondCommand));
-	}
+        assertFalse(findFirstCommand.equals(findSecondCommand));
+    }
 
-	@Test
-	public void execute_noMatchingTags_noPersonFound() {
-		String expectedMessage = FindTagCommand.MESSAGE_TAG_NOT_FOUND;
-		TagsContainKeywordsPredicate predicate = preparePredicate("unknownTag");
-		FindTagCommand command = new FindTagCommand(predicate);
-		expectedModel.updateFilteredPersonList(predicate);
+    @Test
+    public void execute_noMatchingTags_noPersonFound() {
+        String expectedMessage = FindTagCommand.MESSAGE_TAG_NOT_FOUND;
+        TagsContainKeywordsPredicate predicate = preparePredicate("unknownTag");
+        FindTagCommand command = new FindTagCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
 
-		assertCommandSuccess(command, model, expectedMessage, expectedModel);
-		assertEquals(Collections.emptyList(), model.getFilteredPersonList());
-	}
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredPersonList());
+    }
 
-	@Test
-	public void execute_singleMatchingTag_multiplePersonsFound() {
-		String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
-		TagsContainKeywordsPredicate predicate = preparePredicate("friends");
-		FindTagCommand command = new FindTagCommand(predicate);
-		expectedModel.updateFilteredPersonList(predicate);
+    @Test
+    public void execute_singleMatchingTag_multiplePersonsFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 3);
+        TagsContainKeywordsPredicate predicate = preparePredicate("friends");
+        FindTagCommand command = new FindTagCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
 
-		assertCommandSuccess(command, model, expectedMessage, expectedModel);
-		assertEquals(Arrays.asList(ALICE, BENSON, DANIEL), model.getFilteredPersonList());
-	}
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(ALICE, BENSON, DANIEL), model.getFilteredPersonList());
+    }
 
-	@Test
-	public void hashCodeMethod() {
-		TagsContainKeywordsPredicate predicate = new TagsContainKeywordsPredicate(Arrays.asList("friends"));
-		FindTagCommand findTagCommand = new FindTagCommand(predicate);
+    @Test
+    public void hashCodeMethod() {
+        TagsContainKeywordsPredicate predicate = new TagsContainKeywordsPredicate(Arrays.asList("friends"));
+        FindTagCommand findTagCommand = new FindTagCommand(predicate);
 
-		assertEquals(predicate.hashCode(), findTagCommand.hashCode());
-	}
+        assertEquals(predicate.hashCode(), findTagCommand.hashCode());
+    }
 
-	@Test
-	public void toStringMethod() {
-		TagsContainKeywordsPredicate predicate = new TagsContainKeywordsPredicate(Arrays.asList("friends"));
-		FindTagCommand findTagCommand = new FindTagCommand(predicate);
-		String expected = FindTagCommand.class.getCanonicalName() + "{predicate=" + predicate + "}";
+    @Test
+    public void toStringMethod() {
+        TagsContainKeywordsPredicate predicate = new TagsContainKeywordsPredicate(Arrays.asList("friends"));
+        FindTagCommand findTagCommand = new FindTagCommand(predicate);
+        String expected = FindTagCommand.class.getCanonicalName() + "{predicate=" + predicate + "}";
 
-		assertEquals(expected, findTagCommand.toString());
-	}
+        assertEquals(expected, findTagCommand.toString());
+    }
 
-	private TagsContainKeywordsPredicate preparePredicate(String... userInput) {
-		return new TagsContainKeywordsPredicate(Arrays.asList(userInput));
-	}
+    private TagsContainKeywordsPredicate preparePredicate(String... userInput) {
+        return new TagsContainKeywordsPredicate(Arrays.asList(userInput));
+    }
 }
